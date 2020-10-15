ee297b339b3067612d85195c82b479fed782349a
POVILAS <povilas.urbonas85@gmail.com>
2020-10-15 20:52:14 +0100

    added versioned images

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-sudo docker build -t localhost:8082/challenge-3 .
+sudo docker build -t localhost:8082/challenge-3:${BUILD_NUMBER} .
--- scripts/push.sh
+++ scripts/push.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-sudo docker push localhost:8082/challenge-3
+sudo docker push localhost:8082/challenge-3:${BUILD_NUMBER} 
 
